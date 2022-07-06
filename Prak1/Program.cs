9162e1ebae95873d48fa681c52a2801641698602
Николаев Клементий <klim_nikolaev03@mail.ru>
2022-07-06 03:52:55 +0300

    Prak1

--- Prak1/Program.cs
+++ Prak1/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Prak1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter b:");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter c:");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter x:");
+            double x = double.Parse(Console.ReadLine());
+
+            double y = (c > 0 && b == 0) ? (x + a) / (x + c) : (c < 0 || b != 0) ? a * Math.Pow(x, 2) + Math.Pow(b, 2) * x : x / c;
+
+            Console.WriteLine(y);
         }
     }
 }
